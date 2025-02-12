6da5f82bdad1024f0db054d6ffaa05831ed5b09b
Patrick Lawler <patlaw777@icloud.com>
2025-02-12 13:08:33 +0700

    Initial Commit

---
+++ GuardGuysV3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuardGuysV3
+//
+//  Created by Patrick Lawler on 2/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuardGuysV3/GuardGuysV3App.swift
@@ -0,0 +1,17 @@
+//
+//  GuardGuysV3App.swift
+//  GuardGuysV3
+//
+//  Created by Patrick Lawler on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GuardGuysV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
